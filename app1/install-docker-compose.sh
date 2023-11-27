f13c0cd6ab1d018bcfd6776bfd711c48f9636869
Vitorhrds2 <vitor.santos@sou.inteli.edu.br>
2023-11-26 21:15:18 -0300

    add install-docker-compose.sh on folder app1

--- app1/install-docker-compose.sh
+++ app1/install-docker-compose.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
-npm install -g docker-compose
+
+# Script para instalar o docker-compose
+set -e
+
+# Verifique se o script está sendo executado com permissões suficientes
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Este script precisa de permissões de superusuário para instalar o docker-compose."
+  exit 1
+fi
+
+# Verifique se o docker-compose já está instalado
+if command -v docker-compose &> /dev/null; then
+  echo "docker-compose já está instalado."
+  exit 0
+fi
+
+# Instale o docker-compose
+echo "Instalando docker-compose..."
+curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+echo "docker-compose instalado com sucesso."
+
+# Verifique a instalação
+docker-compose --version
